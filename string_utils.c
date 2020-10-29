cb80b2e034bc5e076c9ccff3377fa1c2fc3151db
fikepaci <pacifiquemachine@gmail.com>
2020-10-29 16:58:38 +0200

    update string_utils.c file

--- string_utils.c
+++ string_utils.c
@@ -1,7 +1,5 @@
 #include "holberton.h"
 
-
-
 /**
  * _strlen - returns the length of a string
  * @s: string s
@@ -9,9 +7,10 @@
  */
 int _strlen(char *s)
 {
-
 	int length = 0;
 
+	if (!s)
+		return (0);
 	while (*s)
 	{
 		s++;
@@ -44,8 +43,8 @@ void rev_string(char *s)
 /**
  * _toStr - changes an int to a a string and saves it in nums
  * @n:num
-* @nums:pointer to store the number
-*/
+ * @nums:pointer to store the number
+ */
 void _toStr(unsigned int n, char *nums)
 {
 	if (n < 10)
@@ -58,15 +57,54 @@ void _toStr(unsigned int n, char *nums)
 	_toStr(n / 10, nums);
 }
 
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte
+ * @dest: copied string
+ * @src: string to be copied
+ * Return: pointer to new copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	if (!src)
+	{
+		dest = NULL;
+		return (NULL);
+	}
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (ptr);
+}
 
 /**
- * _printstr - prints a string
- * @c:string to be printed
-* Return:return the length of the string in succes
-* -1 in fauiler
-*/
-int _printstr(char *c)
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
+ */
+char *rot13(char *s)
 {
+	int i;
 
-	return (write(1, c, _strlen(c)));
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
 }
